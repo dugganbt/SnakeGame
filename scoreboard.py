7829ce6592d876f3bf4b1c8491f144e7b77e3f06
dugganbt <99730314+dugganbt@users.noreply.github.com>
2024-04-24 19:44:23 +0200

    High score is now being kept track of during game

--- scoreboard.py
+++ scoreboard.py
@@ -6,18 +6,23 @@ FONT = ("Helvetica", 18, "bold")
 
 class Scoreboard(Turtle):
 
-    def __init__(self):
+    def __init__(self, high_score):
         super().__init__()
         self.hideturtle()
         self.penup()
         self.color("white")
-        self.goto(0, 280)
+        self.goto(0, 270)
         self.score = 0
+        self.high_score = high_score
         self.update_scoreboard()
 
     def update_scoreboard(self):
-        self.clear()
-        self.write(f"Score: {self.score}", align=ALIGNMENT, font=FONT)
+        if self.high_score ==0:
+            self.clear()
+            self.write(f"Score: {self.score}", align=ALIGNMENT, font=FONT)
+        else:
+            self.clear()
+            self.write(f"Score: {self.score} | Highscore: {self.high_score}", align=ALIGNMENT, font=FONT)
 
     def increase_score(self):
         self.score += 1
@@ -25,5 +30,10 @@ class Scoreboard(Turtle):
 
     def reset_score(self):
         self.score = 0
-        self.goto(0, 280)
+        self.goto(0, 270)
         self.update_scoreboard()
+
+    def new_high_score(self):
+        self.high_score = self.score
+        self.update_scoreboard()
+
